6ca78a7356dd8b228e9d9da968de599d72a944c4
Freek Van der Herten <freek@spatie.be>
2022-10-25 12:55:31 +0200

    wip

--- tests/TagsTest.php
+++ tests/TagsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Spatie\OpenTelemetry\Facades\Measure;
+use Spatie\OpenTelemetry\Tests\TestSupport\TestClasses\FakeSpanTagProvider;
 use Spatie\OpenTelemetry\Tests\TestSupport\TestClasses\FakeTraceTagProvider;
 
 it('can add extra tags on the trace level', function() {
@@ -20,5 +21,23 @@ it('can add extra tags on the trace level', function() {
         ->and($payloads[1]['tags']['my-trace-tag'])->toBeString();
 
     expect($payloads[0]['tags']['my-trace-tag'])->toEqual($payloads[1]['tags']['my-trace-tag']);
+});
 
+it('can add extra tags on the span level', function() {
+    config()->set('open-telemetry.span_tag_providers', [FakeSpanTagProvider::class]);
+
+    Measure::startTrace();
+
+    Measure::start('first');
+    Measure::stop('first');
+
+    Measure::start('second');
+    Measure::stop('second');
+
+    $payloads = $this->sentRequestPayloads()['sentSpans'];
+
+    expect($payloads[0]['tags']['my-span-tag'])->toBeString()
+        ->and($payloads[1]['tags']['my-span-tag'])->toBeString();
+
+    expect($payloads[0]['tags']['my-span-tag'])->not()->toEqual($payloads[1]['tags']['my-span-tag']);
 });
